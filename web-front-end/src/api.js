659fb5f2c9f6158bbe06be3198b83214325816cc
Harry Difolco <h@difol.co>
2021-05-24 20:56:35 +0800

    CRUD operations working

--- web-front-end/src/api.js
+++ web-front-end/src/api.js
@@ -28,7 +28,7 @@ function create_todo(text){
     fetch(URL_base,{
         method: 'POST',
         body: JSON.stringify({
-            'text':text
+            "text":text
         }),
         headers: {
             "Content-type": "application/json; charset=UTF-8"
@@ -44,14 +44,24 @@ function create_todo(text){
 }
 
 function update_todo(id, text){
-    fetch(URL_base + `/${id}`,{
-        method: 'PUT',
+    fetch(`https://crsuhfhhz7.execute-api.us-east-1.amazonaws.com/dev/todos/${id}`, {
         body: JSON.stringify({
-            'checked':text,
-        }),
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
+            "text": text,
+            "checked": true }),
+        method: "PUT"
+        })
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+    })
+    .catch((error) => {
+        console.log(error)
+    })    
+}
+
+function delete_todo(text, id){
+    fetch(URL_base + `/${id}`,{
+        method: 'DELETE'
     })
     .then((res) => res.json())
     .then((data) => {
@@ -63,5 +73,5 @@ function update_todo(id, text){
 }
 
 
-update_todo('f1cd4a40-bc84-11eb-af8d-e720a8e429da')
+update_todo("f1cd4a40-bc84-11eb-af8d-e720a8e429da", "foo")
 
