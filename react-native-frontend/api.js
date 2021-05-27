4e94ec3b0bd8d680334f36c1839d97775dc55d55
Harry Difolco <h@difol.co>
2021-05-27 16:48:14 +0800

    error handling

--- react-native-frontend/api.js
+++ react-native-frontend/api.js
@@ -1,58 +1,64 @@
 const URL_base =
-  "https://crsuhfhhz7.execute-api.us-east-1.amazonaws.com/dev/todos";
+  "https://25u289345c.execute-api.us-east-1.amazonaws.com/dev/todos";
+import { DevSettings } from "react-native";
 
 const fetch = require("node-fetch");
+function handleError(error) {
+  if (alert(error + "\nCheck your connection and click ok to reload")) {
+  } else {
+    // referesh app
+    // potentially better solution than this
+    DevSettings.reload();
+  }
+}
 
 async function getAllTodos() {
-  const response = await fetch(URL_base).catch((error) => {
-    console.log(error);
-  });
-  return response.json();
+  try {
+    const response = await fetch(URL_base);
+    return response.json();
+  } catch (error) {
+    handleError(error);
+  }
 }
 
 async function createTodo(text) {
-  const respose = await fetch(URL_base, {
-    method: "POST",
-    body: JSON.stringify({
-      text: text,
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  }).catch((error) => {
-    console.log(error);
-  });
-  return respose.json();
+  try {
+    const respose = await fetch(URL_base, {
+      method: "POST",
+      body: JSON.stringify({
+        text: text,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    return respose.json();
+  } catch (error) {
+    handleError(error);
+  }
 }
 
 function updateTodo(id, checked) {
-  const response = fetch(URL_base + `/${id}`, {
-    body: JSON.stringify({
-      checked: checked,
-    }),
-    method: "PUT",
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
+  try {
+    fetch(URL_base + `/${id}`, {
+      body: JSON.stringify({
+        checked: checked,
+      }),
+      method: "PUT",
     });
-  return response;
+  } catch (error) {
+    handleError(error);
+  }
 }
 
 function deleteTodo(id) {
-  fetch(URL_base + `/${id}`, {
-    method: "DELETE",
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.log(error);
+  try {
+    fetch(URL_base + `/${id}`, {
+      method: "DELETE",
     });
+  } catch (error) {
+    handleError(error);
+  }
 }
 
 export default {
